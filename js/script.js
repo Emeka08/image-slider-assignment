043056d352a54981f8ebf0d67dd57e1f426600a4
Emeka08 <ohaerifavour68@gmail.com>
2020-07-31 21:59:32 +0100

    added a start and stop button

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ let next = document.getElementById('next');
 let previous = document.getElementById('previous');
 let img = document.getElementById('img');
 let dots = document.getElementsByClassName("dot");
+let btn = document.getElementById('button');
 
 let num = 1;
 next.addEventListener('click', function() {
@@ -30,27 +31,45 @@ previous.addEventListener('click', function() {
 
 function scroll(){
 
-for(let i = 1; i <= 1; i++){
+    for(let i = 1; i <= 1; i++){
 
-    if (num <= 0) {
-        num = 4;
-        //img.setAttribute("src", `./img/img${num}.jpg`);
+        if (num <= 0) {
+            num = 4;
+            //img.setAttribute("src", `./img/img${num}.jpg`);
+        }
+        
+        img.setAttribute("src", `./img/img${num}.jpg`);
+        num++;
+        
+        console.log(num);
+
+        if (num >= 5){
+            num = 1;
+        }
+
     }
-    
-    img.setAttribute("src", `./img/img${num}.jpg`);
-    num++;
-    
-    console.log(num);
-
-    if (num >= 5){
-        num = 1;
-    }
-
-}
 
 //setTimeout(scroll,4000);
 }
 
+let interval = '';
 
-setInterval(scroll,3000);
+btn.addEventListener('click', function(){
+    if (btn.innerText === "Start") {
+        //console.log('yea');
+        btn.innerText = "Stop";
+        btn.classList.remove("btn-accept");
+        btn.classList.add("btn-tweet");
+        interval = setInterval(scroll,3000);
+    } else if (btn.innerText === "Stop") {
+        //console.log("no");
+        btn.innerText = "Start";
+        btn.classList.remove("btn-tweet");
+        btn.classList.add("btn-accept");
+        clearInterval(interval);
+    }
+});
+
+
+//setInterval(scroll,3000);
 //scroll();
---
+++ js/script1.js
@@ -0,0 +1,5 @@
+let rand = Math.max(Math.floor(Math.random() * 10 + 1),3);
+
+let max = Math.max(-10,-1,-20,-3);
+
+console.log(rand);
